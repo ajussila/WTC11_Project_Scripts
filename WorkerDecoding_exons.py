ba43d508770ab8afa2d86a2ee59b41fd6895142d
Adam Jussila <jussiladam@gmail.com>
2025-04-20 17:22:37 -0700

    Adding files without the >25MB files

---
+++ WorkerDecoding_exons.py
@@ -0,0 +1,133 @@
+### RHis assumes that the fitting and the dapi feature fitting is complete
+
+def run_single_fov_decoding(save_folder,fov,set_='',lib_fl = r'C:\Users\cfg001\Desktop\WTC11\Adam_exons_1_27_2024_withBlank.csv',redo=False):
+    dec = decoder_simple(save_folder,fov,set_=set_)
+    if not os.path.exists(dec.decoded_fl) or redo:
+        dec.get_XH(dec.fov,set_,ncols=3,nbits=51,th_h=5000,filter_tag = '')#number of colors match 
+        dec.XH = dec.XH[dec.XH[:,-4]>0.25] ### keep the spots that are correlated with the expected PSF for 60X
+        dec.load_library(lib_fl,nblanks=-1)
+        dec.ncols = 3
+        get_intersV2(dec,nmin_bits=3,dinstance_th=2,enforce_color=True,enforce_set=17*3,redo=False)
+        get_icodesV3(dec,nmin_bits=3,iH=-3) ### saves a decoded....npz
+        print("Decoding completed for {}.".format(dec.fov))
+
+import sys
+sys.path.append(r'C:\Users\cfg001\Desktop\WTC11\NMERFISH')
+from ioMicro import *
+#from ioMicro import *
+all_flds = glob.glob(r'W:\Adam\E211_WTC11_day14_Rad21\H*_RMER_Q*')
+all_flds = np.array(all_flds)[np.argsort([get_iH(fld) for fld in all_flds])]
+all_flds
+print("Folders found:",len(all_flds))
+fov = 'Conv_zscan__200.zarr'
+save_folder = r'W:\Adam\E211_WTC11_day14_Rad21\Analysis_Deconvolve_CG\RNA_Exons'
+
+
+def fov_to_dapi_features(fov='Conv_zscan__010',save_folder_ref=r'W:\Adam\E211_WTC11_day14_Rad21\Analysis_Deconvolve_CG\RNA_Exons', save_folder=r'W:\Adam\E211_WTC11_day14_Rad21\Analysis_Deconvolve_CG\RNA_Exons',tag_new='H1_RMER_Q1',tag_ref = 'H1_DMER_1'):
+    fov_ = fov.replace('.zarr','')
+    fl = save_folder+os.sep+fov_+'--'+tag_new+'--dapiFeatures.npz'
+    fl_ref = save_folder_ref+os.sep+fov_+'--'+tag_ref+'--dapiFeatures.npz'
+    return fl,fl_ref
+def load_segmentation(dec,segmentation_folder = r'W:\Adam\E211_WTC11_day14_Rad21\Analysis_Deconvolve_CG\segmentationDAPI',segm_tag='H1_DMER_1'):
+    fl_segm = segmentation_folder+os.sep+dec.fov.replace('.zarr','')+'--'+segm_tag+'--CYTO_segm_expanded.npz'
+    segm,shape = np.load(fl_segm)['segm'],np.load(fl_segm)['shape']
+    #segm_ = resize(segm,shape)
+    dec.im_segm_ = segm
+    dec.shape=shape
+    dec.segm_tag=segm_tag
+def get_dic_drift(dec):
+    drifts,flds,fov_,fl_ref = np.load(dec.drift_fl,allow_pickle=True)
+    return {os.path.basename(fld):drft[0] for drft,fld in zip(drifts,flds)}
+def main_f_fov(save_folder =r'W:\Adam\E211_WTC11_day14_Rad21\Analysis_Deconvolve_CG\RNA_Exons',fov='Conv_zscan__200',set_ = '',ncols=3,
+          scores_ref_fl=r'W:\Adam\E211_WTC11_day14_Rad21\Analysis_Deconvolve_CG\RNA_Exons\scoresRef.pkl',th=-1.5,force=False,segm_tag='H1_RMER_Q1',tag_new='H1_RMER_Q1'):
+    save_fld_cell = os.path.dirname(save_folder)+os.sep+'best_AdamExonsNew'
+    if not os.path.exists(save_fld_cell): os.makedirs(save_fld_cell)
+    save_fl = save_fld_cell+os.sep+fov+'__XHfs_finedrft.npz'
+    if not os.path.exists(save_fl) or force:
+        dec = decoder_simple(save_folder,fov,set_)
+        print(dec.decoded_fl)
+        dec.ncols = ncols
+        dec.load_decoded()
+        print(dec.decoded_fl)
+        dec.save_fl=save_fl
+        #apply_fine_drift(dec,plt_val=True)
+        #dec.save_folder= r'C:\Users\cfg001\Desktop\WTC11\flat_field'
+        #apply_flat_field(dec,tag='Scope4_med_col_raw')
+        #dec.save_folder= save_folder
+        #scoresRefT = get_score_per_color(dec)
+        scoresRefT = pickle.load(open(scores_ref_fl,'rb'))
+        dec.dist_best = np.load(dec.decoded_fl)['dist_best']
+        get_score_withRef(dec,scoresRefT,plt_val=True,gene=None,iSs = None,th_min=-7.5,include_dbits=True)
+        dec.th=th
+        plot_statistics(dec)
+        
+        #threshold the combined EM score
+        keep = dec.scoreA>dec.th
+        dec.XH_prunedf,dec.icodesNf=dec.XH_pruned[keep],dec.icodesN[keep]
+        nbits = dec.XH_prunedf.shape[1]
+        dec.XH_prunedF = np.concatenate([dec.XH_prunedf,np.repeat(dec.icodesNf,nbits).reshape(-1,nbits)[:,:,np.newaxis]],axis=-1)
+        ### get the drift - to correct to the segmentation space
+        dec.dic_drift = get_dic_drift(dec)
+        
+        load_segmentation(dec)
+        dec.im_segm_ = expand_segmentation(dec.im_segm_, nexpand=5)
+        
+        ### Compute drift between the segmentation file and the reference drift file
+        fl,fl_ref = fov_to_dapi_features(tag_new=tag_new,tag_ref=segm_tag)
+        txyz_segm = get_best_translation_points_dapiFeat(fl,fl_ref,resc=5,th=5)[0]
+        tzxy_seg = -txyz_segm#dec.dic_drift[dec.segm_tag] #np.round([dic_drift[key][0] for key in dic_drift if cp.segm_tag in key]).astype(int)
+        
+        ### Augment the fitting data with cell id
+        resc = dec.im_segm_.shape/dec.shape
+        XH_ = dec.XH_prunedF.copy()
+        XH_[:,:,:3] = XH_[:,:,:3]-tzxy_seg ### bring fits to cell segmentation space - modified to -
+        XC = (np.nanmean(XH_[:,:,:3],axis=1)*resc).astype(int) #rescale to segmentation size
+        dec.XC = XC
+        keep = np.all(XC>=0,axis=-1)&np.all(XC<dec.im_segm_.shape,axis=-1)
+        icells = np.zeros(len(XC))
+        icells[keep]=dec.im_segm_[tuple(XC[keep].T)]
+        nbits = XH_.shape[1]
+        icells = np.repeat(icells,nbits).reshape(-1,nbits)[:,:,np.newaxis]
+        XH_f = np.concatenate([XH_,icells],axis=-1)
+        dec.XH_f=XH_f
+        #XH_fs=keep_best_per_cell_fast(XH_f,nbest=20)
+        
+        np.savez(dec.save_fl,XH_fs=XH_f)
+        return dec
+
+
+
+
+def main_f(fov,try_mode=False):
+    set_=''
+    
+    
+    if True:#not os.path.exists(fl):
+        if try_mode:
+            try:
+                compute_drift_V2(save_folder,fov,all_flds,set_='',redo=False,gpu=True)
+                run_single_fov_decoding(save_folder,fov)
+                dec = main_f_fov(fov=fov,th=-1.5,force=False)
+            except:
+                print("Failed within the main analysis:",fov)
+        else:
+            compute_drift_V2(save_folder,fov,all_flds,set_='',redo=False,gpu=True)
+            run_single_fov_decoding(save_folder,fov)
+            dec = main_f_fov(fov=fov,th=-1.5,force=False)
+    
+    
+    return fov
+    
+from multiprocessing import Pool, TimeoutError
+    
+if __name__ == '__main__':
+    # start 4 worker processes
+    fovs = [os.path.basename(fl) for fl in np.sort(glob.glob(all_flds[0]+os.sep+'*.zarr'))]
+    fovs = [ fov.split(".zarr")[0] for fov in fovs ]
+    item = fovs[100]
+    #main_f(item,try_mode=False)
+    if True:
+        with Pool(processes=4) as pool:
+            print('starting pool')
+            result = pool.map(main_f, fovs)
+#conda activate cellpose2&&python D:\Carlos\NMERFISH\WorkerDecodingD111_exons.py
---
+++ workerADAM_CTCF_DNAd14_combBogdanVerified.py
@@ -0,0 +1,167 @@
+#!/usr/bin/python
+
+#
+#
+######### Please add documentation
+### This is for ADAMs project, it will be running the coimbinatorial imaging, using the fitting/dapi-feats algorithm with PSFs and Flat Field Corrections sourced from Scope4 (Bogdans Microscope)
+### Started on 6/26/2024 at 10:22 am
+### python workerADAMRAD21_CHATDNAd14_comb.py
+#################################################################
+from multiprocessing import Pool, TimeoutError
+import time,sys
+import os,sys,numpy as np
+# change this: 
+#C:\Users\cfg001\Desktop\WTC11
+master_analysis_folder = r'C:\Users\cfg001\Desktop\WTC11'
+sys.path.append(master_analysis_folder)
+from ioMicroBogdanVerified import *
+#standard is 4, its number of colors +1 
+ncols = 4
+
+#change
+#r'C:\Users\cfg001\Desktop\WTC11\flat_field\Scope4_med_col_raw'
+psf_file = r'C:\Users\cfg001\Desktop\WTC11\psfs\psf_750_E217_H1_MAP2_EEF2_DCX_Scope4.npy'
+
+#W:\Adam\E217_WTC11_day14_Rad21new
+save_folder = r'W:\Adam\E218_WTC11_day14_CTCF_DNA_MERFISH\AnalysisDeconvolve_CGBogdanV'
+#change to whatever Bogdan gives you
+#C:\Users\cfg001\Desktop\WTC11\flat_field
+flat_field_fl = r'C:\Users\cfg001\Desktop\WTC11\flat_field\Scope4_med_col_raw'
+
+
+def compute_drift_features(save_folder,fov,all_flds,set_,redo=False,gpu=True):
+    fls = [fld+os.sep+fov for fld in all_flds]
+    for fl in fls:
+    	#important to update here, not on ioMicro for the flat field, psf, etc...
+        get_dapi_features(fl,save_folder,set_,gpu=gpu,im_med_fl = flat_field_fl+r'3.npz',
+                    psf_fl = psf_file)
+def main_do_compute_fits(save_folder,fld,fov,icol,save_fl,psf,old_method,icol_flat):
+    '''
+    Inputs: save_folder -> this is on the NAS,where the output is saved
+    
+    '''
+    im_ = read_im(fld+os.sep+fov)
+    im__ = np.array(im_[icol],dtype=np.float32)
+    
+    if old_method:
+        ### previous method without deconvolution
+        im_n = norm_slice(im__,s=30)
+        #Xh = get_local_max(im_n,500,im_raw=im__,dic_psf=None,delta=1,delta_fit=3,dbscan=True,
+        #      return_centers=False,mins=None,sigmaZ=1,sigmaXY=1.5)
+        Xh = get_local_maxfast_tensor(im_n,th_fit=500,im_raw=im__,dic_psf=None,delta=1,delta_fit=3,sigmaZ=1,sigmaXY=1.5,gpu=False)
+    else:
+        
+        fl_med = flat_field_fl+str(icol)+'.npz'
+
+        im_med = np.array(np.load(fl_med)['im'],dtype=np.float32)
+        im_med = cv2.blur(im_med,(20,20))
+        im__ = im__/im_med*np.median(im_med)
+
+        Xh = get_local_max_tile(im__,th=3600,s_ = 500,pad=100,psf=psf,plt_val=None,snorm=30,gpu=True,
+                                deconv={'method':'wiener','beta':0.0001},
+                                delta=1,delta_fit=3,sigmaZ=1,sigmaXY=1.5)
+        
+    np.savez_compressed(save_fl,Xh=Xh)
+def compute_fits(save_folder,fov,all_flds,redo=False,ncols=ncols,
+                psf_file = psf_file,try_mode=True,old_method=False,redefine_color=None):
+    
+    psf = np.load(psf_file)
+    for ifld,fld in enumerate(tqdm(all_flds)):
+        if redefine_color is not None:
+            ncols = len(redefine_color[ifld])
+        for icol in range(ncols-1):
+            ### new method
+            if redefine_color is None:
+                icol_flat = icol
+            else:
+                #print("ifld is: "+str(ifld))
+                #print("icol is: "+str(icol))
+                icol_flat = redefine_color[ifld][icol]
+            tag = os.path.basename(fld)
+            save_fl = save_folder+os.sep+fov.split('.')[0]+'--'+tag+'--col'+str(icol)+'__Xhfits.npz'
+            if not os.path.exists(save_fl) or redo:
+                if try_mode:
+                    try:
+                        main_do_compute_fits(save_folder,fld,fov,icol,save_fl,psf,old_method,redefine_color)
+                    except:
+                        print("Failed",fld,fov,icol)
+                else:
+                    main_do_compute_fits(save_folder,fld,fov,icol,save_fl,psf,old_method,redefine_color)
+                    
+
+def main_f(set_ifov,try_mode=True):
+    '''
+    CHANGE PATH HERE
+    '''
+    #20230122_R120PVTS32RDNA for the data used in grant
+    #r'\\merfish6\merfish6v2\DNA_FISH\20230212_R120PVTS32RDNABDNF\Analysis'
+     ### save folder
+    if not os.path.exists(save_folder): os.makedirs(save_folder)
+    set__ = ''
+   
+    all_flds = []
+    redefine_color = []
+   
+    
+    
+    #change to Adam's NAS, using H*DNA for the naming scheme
+    #all_flds_ = glob.glob(r'/mnt/Ren-Imaging_NAS/Adam/E200_WTC11_WTday3__8_31_2023/H*DMER*'+set__) ################################# modify for 3-col small tad 
+    #redefine_color += [[0,1,2,3]]*len(all_flds_) #### three signal color
+    #all_flds+=all_flds_
+
+    #toAdd: H*RMER, mrna (Q), I
+
+    #W:\Adam\E218_WTC11_day14_CTCF_DNA_MERFISH
+    all_flds_ = glob.glob(r'W:\Adam\E218_WTC11_day14_CTCF_DNA_MERFISH\H*DMER*'+set__) ################################# modify for 3-col small tad 
+    redefine_color += [[0,1,2,3]]*len(all_flds_) #### three signal color
+    all_flds+=all_flds_
+
+    
+    
+    
+   
+    set_,ifov = set_ifov
+    all_flds = [fld.replace(set__,set_) for fld in all_flds]
+    fovs_fl = save_folder+os.sep+'fovs__'+set_+'.npy'
+    #print(fovs_fl)
+    #print(all_flds)
+    if not os.path.exists(fovs_fl):
+        fls = glob.glob(all_flds[0]+os.sep+'*.zarr')
+        fovs = [os.path.basename(fl) for fl in fls]
+        np.save(fovs_fl,fovs)
+    else:
+        fovs = np.load(fovs_fl)
+    if ifov<len(fovs):
+        fov = fovs[ifov]
+        
+        print("Computing fitting on: "+str(fov))
+        print(len(all_flds),all_flds)
+        compute_fits(save_folder,fov,all_flds,redo=False,try_mode=try_mode,redefine_color=redefine_color)
+        print("Computing drift on: "+str(fov))
+        compute_drift_features(save_folder,fov,all_flds,set_,redo=False,gpu=True)
+        #compute_drift(save_folder,fov,all_flds,set_,redo=False)
+       
+        #compute_decoding(save_folder,fov,set_)
+        
+    return set_ifov
+if __name__ == '__main__':
+    # start 4 worker processes
+    items = [(set_,ifov)for set_ in ['']
+                        for ifov in range(301)]
+    #items.reverse()
+    print(items)
+    main_f(("",20),try_mode=False)
+    if True:
+        with Pool(processes=6) as pool:
+            print('starting pool')
+            result = pool.map(main_f,items)
+            try:
+                #for i in range(0,166):
+                print("engaging with try mode...")
+                result = pool.map(main_f,items)
+            except:
+                    print("failed at: " )
+
+            
+##conda activate napari-env
+##python C:\Users\cfg001\Desktop\WTC11\workerADAM_CTCF_DNAd14_combBogdanVerified.py
